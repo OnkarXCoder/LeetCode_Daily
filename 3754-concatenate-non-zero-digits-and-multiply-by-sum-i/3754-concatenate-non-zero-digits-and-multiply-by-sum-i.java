dfc878630dc67e2bf911df0e2f81fe71fef2e7cf
Onkar Venkatesh Nagul <106875658+OnkarXCoder@users.noreply.github.com>
2026-07-07 19:28:32 +0530

    Time: 1 ms (99.85%), Space: 42.5 MB (67.71%) - LeetHub

---
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long sumAndMultiply(int n) {
+        int x=0,sum=0;
+        int p=1;
+        while(n>0){
+            int d=n%10;
+            if(d!=0){
+                x+=d*p;
+                sum+=d;
+                p*=10;
+            }
+            n/=10;
+        }
+        return 1L*x*sum;
+    }
+}
