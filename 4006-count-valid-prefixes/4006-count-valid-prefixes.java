30430ca6dde617e9e5ba554c1c559e75b4f1a6a5
Onkar Venkatesh Nagul <106875658+OnkarXCoder@users.noreply.github.com>
2026-08-01 22:05:33 +0530

    Time: 1 ms (100%), Space: 44.2 MB (9.63%) - LeetHub

---
+++ 4006-count-valid-prefixes/4006-count-valid-prefixes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countValidPrefixes(String s) {
+        int zero=0,one=0;
+        int ans=0;
+        for(char ch:s.toCharArray()){
+            if(ch=='0'){
+                zero++;
+            }else{
+                one++;
+            }
+            if(Math.abs(zero-one)<=1){
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
